156bdfa4430fdc397f9268f0359ae955be7cc5ca
Fardin <fardinfahim1@gmail.com>
2022-09-08 22:00:55 +0200

    this file has changed

--- sumNumber.py
+++ sumNumber.py
@@ -3,12 +3,11 @@
 # if the user entered 10 the output should be 55 (1+2+3+4+5+6+7+8+9+10)
 
 
+
 print("-------------------- Start Program -----------------------")
 number = int(input("plz Enter your Number for Calculate=  "))
-
+result = 0
 for number1 in range(number):
-    
-    number1 = number + 2
-    print("This is your Result ", number1)
+    result += number1
+print("This is your Result ", result)
 print("-------------------- End of Program -----------------------")
-
