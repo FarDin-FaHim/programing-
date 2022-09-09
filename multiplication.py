10da7d7c1f0023a3893b23ba80ce870392ea6a8c
Fahim <fardinfahim1@gmail.com>
2022-09-09 22:07:06 +0200

    Fardin Test5

--- multiplication.py
+++ multiplication.py
@@ -8,5 +8,5 @@ n2 = float(input("Insert the second number:  "))
 
 print("First number x  Secend number is ====> ",
       numbers1*n2)
-
+print("Fardin Test")
 print("--------------- End of The Program----------------")
