9eeb677564ce1245cfb61244f84d7c2d935fe28f
Fahim <fardinfahim1@gmail.com>
2022-09-09 21:49:44 +0200

    This is Test3

--- floating.py
+++ floating.py
@@ -1,4 +1,7 @@
 # Display float number with 2 decimal places using print()
+# Hallo This is test
+
 number = 54.3
 total = int(number)
 print(total)
+print("hallo This is Test")
---
+++ justforExercise.py
@@ -0,0 +1,9 @@
+a = 20
+b = 20
+c = 4
+d = 8
+e = 20
+
+print ("a:%d b:%d c:%d d:%d" % (a,b,c,d))
+print("This is just for Test")
+
