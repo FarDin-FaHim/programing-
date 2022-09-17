e055d30dda1fb38b99ab57f46e380fc8ef95fa25
Fahim <fardinfahim1@gmail.com>
2022-09-17 15:22:58 +0200

    add ip to binary proggram

---
+++ ip_to_binary.py
@@ -0,0 +1,5 @@
+print("--------------- This Program convert ip to Binary ------------")
+ip = int((input("Enter your IP ==> :")))
+print(bin(ip))
+
+print("-------------------End of program----------------------------")
