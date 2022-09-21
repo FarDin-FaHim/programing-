f40fdbb785fdebe884b973c14b168bd9c6a17587
Fahim <fardinfahim1@gmail.com>
2022-09-21 22:46:14 +0200

    Add upper and lower funcation

---
+++ upper_and_lower_funcation.py
@@ -0,0 +1,13 @@
+# This Function just Convert  Uppercase to lowercase und lowercase to Uppercase
+def upper(text):
+
+   print(text.upper())
+
+def lower(text1):
+    print(text1.lower())
+    
+
+upper(input("Enter your name to change Lower to Upper:" ))
+lower(input("Enter your name to change Upper to Lower :" ))
+
+
