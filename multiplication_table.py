9d2ead20edf923d25a65bd106ad777604ccdf1b6
Fahim <fardinfahim1@gmail.com>
2022-10-01 22:47:21 +0200

    add Multiplication table

---
+++ multiplication_table.py
@@ -0,0 +1,42 @@
+
+# This program print all multiplication table from 1 up to 10 
+def tabel (x):
+    y = 1
+    while(y<11):
+        print(x,"x",y,"=",x*y,sep="")
+        y = y+1
+        if ( y==11):
+            print("............")
+            print("The multiplication table of", a+1, " is:\n")
+            
+
+
+
+print("     ")
+a = 1
+while(a < 11):
+    tabel(a)
+    a = a +1
+
+
+
+#f = int(input("plz Enter your number: "))
+#tabel(f)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
