c5b2eb529ffd9401602f7c53c8cdced64d4b3570
Fahim <fardinfahim1@gmail.com>
2022-10-02 00:02:58 +0200

    add multiply number in list

---
+++ multiply_number_list.py
@@ -0,0 +1,17 @@
+# This program mulitiply all the numbers in a list
+#Method 1
+my_list = [4,5,6,7]
+result = 1
+for i in my_list:
+    result = result * i
+    print(result)
+
+
+
+# Method 2
+#for i in range(len(my_list)):
+ #   result = result * my_list[i]
+  #  print(result)
+
+
+    
